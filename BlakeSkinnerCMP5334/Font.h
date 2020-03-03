727b4fe4227b677a4f3d00a72472be6ed67991bf
Skinnerblake99 <56030008+Skinnerblake99@users.noreply.github.com>
2020-03-03 12:55:24 +0000

    Working through lab
    on the lab for text rendering

---
+++ BlakeSkinnerCMP5334/Font.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <glm/glm.hpp>
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Font {
+	friend class GUIText;
+	friend class ModelRenderer;
+public:
+	Font(string fontTexture) {
+		this->fontTexture = fontTexture;
+		this->texture = new Texture(fontTexture);
+	}
+
+	virtual ~Font() {
+		delete texture;
+	}
+};
